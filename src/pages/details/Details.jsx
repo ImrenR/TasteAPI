eb0b1fb2b652b5fd66b130b82d474c311ad18e58
ImrenR <imrenrahbay@gmail.com>
2025-10-21 13:22:55 +0100

    update

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,19 @@
+import { DetailContainer, DetailPart, HeaderContainer, ImgContainer, IngredContainer, OtherPart } from "./DetailStyles"
+
+const Details = () => {
+  return (
+    <DetailContainer>
+<DetailPart>
+<ImgContainer src={""}/>
+
+<HeaderContainer></HeaderContainer>
+<IngredContainer></IngredContainer>
+<OtherPart></OtherPart>
+
+</DetailPart>
+
+    </DetailContainer>
+  )
+}
+
+export default Details
