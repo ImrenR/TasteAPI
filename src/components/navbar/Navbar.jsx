9383828cd555fc631892c7b3b30db8092d474e8c
ImrenR <imrenrahbay@gmail.com>
2025-10-21 13:19:31 +0100

    update

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Hamburger, Logo, Menu, MenuLink, Nav } from "./NavbarStyles"
+
+const Navbar = () => {
+  return (
+    <Nav>
+
+<Menu>
+<MenuLink>
+  <Logo></Logo>
+</MenuLink>
+<Hamburger></Hamburger>
+
+
+</Menu>
+
+
+
+
+    </Nav>
+  )
+}
+
+export default Navbar
