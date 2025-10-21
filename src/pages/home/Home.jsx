cae3a2866aa9c3dc8be2cee568afeaee473c5b11
ImrenR <imrenrahbay@gmail.com>
2025-10-21 13:24:59 +0100

    update

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import { Cards, MainContainer, RecipeButton, RecipeHeader, RecipeImage } from "./HomeStyles"
+
+const Home = () => {
+  return (
+<MainContainer>
+
+<Cards>
+<RecipeHeader></RecipeHeader>
+<RecipeImage src=""/>
+<RecipeButton>Details</RecipeButton>
+
+</Cards>
+
+</MainContainer>
+
+  )
+}
+
+export default Home
