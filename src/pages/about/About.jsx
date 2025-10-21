d24f2258a19d9f106dbb1cd68c777c42da96583f
ImrenR <imrenrahbay@gmail.com>
2025-10-21 13:21:10 +0100

    update

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,19 @@
+import { AboutContainer, HeaderContainer, InfoContainer, StyledImage } from "./AboutStyles"
+
+const About = () => {
+  return (
+
+<AboutContainer>
+<StyledImage src=""/>
+<HeaderContainer>
+
+</HeaderContainer>
+<InfoContainer>
+  
+</InfoContainer>
+</AboutContainer>
+
+  )
+}
+
+export default About
