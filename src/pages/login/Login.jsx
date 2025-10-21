dba3e0c514cfd815f9aeb7ef0905776a9866adc7
ImrenR <imrenrahbay@gmail.com>
2025-10-21 13:27:27 +0100

    update

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+import { FormContainer, Header, LoginContainer, StyledButton, StyledForm, StyledImg, StyledInput } from "./LoginStyles"
+
+const Login = () => {
+  return(
+
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src=""/>
+        <Header></Header>
+              </FormContainer>
+      <StyledForm>
+<StyledInput placeholder=""/>
+<StyledButton></StyledButton>
+      </StyledForm>
+    </LoginContainer>
+  )
+}
+
+export default Login
