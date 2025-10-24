d38c782145baa72e42e3c3a4989e87025b32c5f5
ImrenR <imrenrahbay@gmail.com>
2025-10-24 18:23:27 +0100

    updatey

--- src/pages/errorPage/NotFound.jsx
+++ src/pages/errorPage/NotFound.jsx
@@ -1,9 +1,24 @@
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Main from './NotFoundStyle'
+import { Navigate } from 'react-router-dom';
 
 const NotFound = () => {
 
-  const [sayac, setsayac] = useState(5);
+  const [sayac, setSayac] = useState(5);
+
+  useEffect(() => {
+
+    const timer = setInterval(()=> {
+    setSayac((sayac)=> (sayac===0 ? sayac : sayac-1 ))
+  },1000)
+
+  if(sayac===0) clearInterval(timer)
+  }, [])
+  
+ if(sayac===0){
+  return <Navigate to="/home"/>;
+ }
+
 
   return (
   <Main>
