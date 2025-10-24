391e85e7287c10d7ff6ddef170b118db1485025b
ImrenR <imrenrahbay@gmail.com>
2025-10-24 17:22:37 +0100

    up

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,16 +1,40 @@
-
-import { useLocation } from 'react-router-dom'
-
+import { useLocation } from "react-router-dom";
+import {
+  DetailContainer,
+  DetailPart,
+  HeaderContainer,
+  ImgContainer,
+  IngredContainer,
+  OtherPart,
+} from "./DetailsStyles";
+import diet from "../../assets/diet.svg";
 const Details = () => {
+  const {
+    state: { recipe },
+  } = useLocation();
 
-  const { state :
-    {recipe}
-  } = useLocation()
-
-  
   return (
-    <div>Details</div>
-  )
-}
+    <DetailContainer>
+      <HeaderContainer>
+        <img src={diet} alt="" />
+      </HeaderContainer>
 
-export default Details
+      <DetailPart>
+        <OtherPart>
+          
+          <span> {recipe.strMeal}</span>
+          <h2>Ingredients</h2>
+          <span>{recipe.strIngredient1}</span>
+          <span>{recipe.strIngredient2}</span>
+          <span>{recipe.strIngredient3}</span>
+          <span>{recipe.strIngredient4}</span>
+          <span>{recipe.strIngredient5}</span>
+          <span>{recipe.Instructions}</span>
+        </OtherPart>
+        <ImgContainer src={recipe.strMealThumb} width="480px" />
+      </DetailPart>
+    </DetailContainer>
+  );
+};
+
+export default Details;
