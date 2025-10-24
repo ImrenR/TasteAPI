3eba9cc6667269b94f54822e6bede2f60e7fdb22
ImrenR <imrenrahbay@gmail.com>
2025-10-24 18:08:13 +0100

    update

--- src/pages/errorPage/NotFound.jsx
+++ src/pages/errorPage/NotFound.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Main from './NotFoundStyle'
 
 const NotFound = () => {
+
+  const [sayac, setsayac] = useState(5);
+
   return (
   <Main>
   <h3>Oops!... Something went wrong</h3>
-  <p>Within <strong> 5 </strong> seconds you will be redirected to the home page!</p>
+  <p>Within <strong> {sayac} </strong> seconds you will be redirected to the home page!</p>
   </Main>
   )
 }
